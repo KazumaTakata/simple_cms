d2554444dbf373dfb440c5b63a738bcafee03aa5
Kazuma Takata <neilbuzztakata@gmail.com>
2018-10-27 20:44:41 +0900

    first commit

---
+++ template/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="static/index.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>rr</h1>
+    <form  action="/login"  action="post">
+First name: <input type="text" name="fname"><br>
+  Last name: <input type="text" name="lname"><br>
+  <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
